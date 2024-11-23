fcd3211f7aef8970fc614aa8cc9b371c57fc8139
dnxa <kuridze0@gmail.com>
2024-11-23 12:57:48 +0400

    Make some fields necessary to fill out

--- l18/products/models.py
+++ l18/products/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
-    quantity = models.IntegerField()
-    quality = models.IntegerField()
+    name = models.CharField(max_length=100, null=False, blank=False)
+    type = models.CharField(max_length=100, null=False, blank=False)
+    quantity = models.IntegerField(null=False, blank=False)
+    quality = models.IntegerField(null=False, blank=False)
     timestamp = models.DateField(auto_now=True)
 
     def __str__(self):
         return "name: " + str(self.name) + " type: " + str(self.type) + " time:" + str(self.timestamp)
 
 class LoyalCustomer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
 
     timestamp = models.DateField(auto_now=True)
 
-    products = models.ForeignKey("Product", on_delete=models.CASCADE)
+    products = models.ForeignKey("Product", on_delete=models.CASCADE, null=False, blank=False)
 
 
     def __str__(self):
