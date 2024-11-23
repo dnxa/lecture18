3c3047be71d7fd1b4a6f5a68b54c2d40453124bf
dnxa <kuridze0@gmail.com>
2024-11-23 12:33:14 +0400

    Add products model with fields

--- l18/products/models.py
+++ l18/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    quality = models.IntegerField()
+    timestamp = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return "name: " + str(self.name) + " type: " + str(self.type) + " time:" + str(self.timestamp)
